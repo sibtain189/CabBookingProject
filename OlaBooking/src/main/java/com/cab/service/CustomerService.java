c5c5d46eada30a4909130b4314492557096282c9
Sibtain Ahmad <sibtainahmad976@gmail.com>
2022-06-10 12:24:50 +0530

    Change id to auto generated

--- OlaBooking/src/main/java/com/cab/service/CustomerService.java
+++ OlaBooking/src/main/java/com/cab/service/CustomerService.java
@@ -11,27 +11,24 @@ import java.util.List;
 import java.util.Objects;
 
 @Service
-public class CustomerService implements CustomerServiceInterface {
+public class CustomerService implements CustomerServiceInterface{
 
-    @Autowired
-    private CustomerDAO customerRepositry;
+	private CustomerDAO customerRepositry;
+	
+	@Override
+	public Customer insertCustomer(Customer customer) {
+		 return customerRepositry.save(customer);
+	}
 
-    @Override
-    public List<TripBooking> getList(Integer customerId) throws CustomerException{
-        Customer customer = customerRepositry.findById(customerId).orElseThrow(() -> new CustomerException("Customer doesn't exist with this Id" + customerId));
+	@Override
+	public List<TripBooking> getList(Integer customerId) throws CustomerException {
+		Customer customer = customerRepositry.findById(customerId).orElseThrow(() -> new CustomerException("Customer doesn't exist with this Id" + customerId));
         return  customer.getTripBookings();
-    }
+	}
 
-    @Override
-    public Customer insertCustomer(Customer customer) throws CustomerException {
-    
-    	
-        return customerRepositry.save(customer);
-    }
-
-    @Override
-    public Customer updateCustomer(Customer customer, Integer id) throws CustomerException {
-        Customer customerDb = customerRepositry.findById(id)
+	@Override
+	public Customer updateCustomer(Customer customer, Integer customerId) throws CustomerException {
+		Customer customerDb = customerRepositry.findById(customerId)
                 .orElseThrow(() -> new CustomerException("Admin doesn't exist with id : " + customer.getUserId()));
 
         if (Objects.nonNull(customer.getUsername()) &&
@@ -56,33 +53,35 @@ public class CustomerService implements CustomerServiceInterface {
 
 
         return customerRepositry.save(customerDb);
-    }
+	}
 
-    @Override
-    public Customer deleteCustomer(Integer customerId) throws CustomerException {
-        Customer customer = customerRepositry.findById(customerId).orElseThrow(() -> new CustomerException("Customer doesn't exist with this Id" + customerId));
+	@Override
+	public Customer deleteCustomer(Integer customerId) throws CustomerException {
+		Customer customer = customerRepositry.findById(customerId).orElseThrow(() -> new CustomerException("Customer doesn't exist with this Id" + customerId));
         customerRepositry.delete(customer);
         return customer;
-    }
+	}
 
-    @Override
-    public List<Customer> viewCustomers() {
-        return customerRepositry.findAll();
-    }
+	@Override
+	public List<Customer> viewCustomers() {
+		return customerRepositry.findAll();
+	}
 
-    @Override
-    public Customer viewCustomer(Integer customerId) throws CustomerException {
-        Customer customer = customerRepositry.findById(customerId).orElseThrow(() -> new CustomerException("Customer doesn't exist with this ID " + customerId));
+	@Override
+	public Customer viewCustomer(Integer customerId) throws CustomerException {
+		Customer customer = customerRepositry.findById(customerId).orElseThrow(() -> new CustomerException("Customer doesn't exist with this ID " + customerId));
         return customer;
-    }
+	}
 
-    @Override
-    public Customer validateCustomer(String username, String password) throws CustomerException {
-    	
+	@Override
+	public Customer validateCustomer(String username, String password) throws CustomerException {
+
         Customer customer = customerRepositry.validateCustomer(username, password);
         if (customer == null)
             throw new CustomerException("Customer doesn't Exist... Sorry!");
         else
             return customer;
-    }
+	}
+
+	
 }
